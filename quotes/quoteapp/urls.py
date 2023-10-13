1b6634fa41e422e291b3cf3d965cdf69555884f6
Strategs <127782103+Strategs@users.noreply.github.com>
2023-10-13 20:59:23 +0300

    Final commit

--- quotes/quoteapp/urls.py
+++ quotes/quoteapp/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('tag/', views.tag, name='tag'),
     path('author/', views.author, name='author'),
     path('quote/', views.quote, name='quote'),
+    path('scraper/', views.start_scraping, name='start_scraping'),
+    path('about/<int:author_id>', views.about_author, name='about_author'),
+    path('quotes_by_tag/<str:tag_name>', views.quotes_by_tag, name='quotes_by_tag'),
+
 ]
--- quotes/quoteapp/views.py
+++ quotes/quoteapp/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect, get_object_or_404
+
 from .forms import TagForm, AuthorForm, QuoteForm
 from .models import Tag, Author, Quote
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+
+from . import scraper
+
 
 # Create your views here.
 
 
 def main(request):
     quotes = Quote.objects.all()
-    authors = Author.objects.all()
+    paginator = Paginator(quotes, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    # authors = Author.objects.all()
     tags = Tag.objects.all()
+    tag_counts = Tag.objects.annotate(quote_count=Count('quote')).order_by('-quote_count')[:10]
+    return render(request, 'quoteapp/index.html', {'page_obj': page_obj, "quotes": quotes, "tag": tags, 'author': author, 'top_tags': tag_counts})
 
-    return render(request, 'quoteapp/index.html', {"quotes": quotes, "tag": tags})
 
-
+@login_required
 def tag(request):
     if request.method == 'POST':
         form = TagForm(request.POST)
@@ -25,6 +36,7 @@ def tag(request):
     return render(request, 'quoteapp/tag.html', {'form': TagForm()})
 
 
+@login_required
 def author(request):
     if request.method == 'POST':
         form = AuthorForm(request.POST)
@@ -37,6 +49,7 @@ def author(request):
     return render(request, 'quoteapp/author.html', {'form': AuthorForm()})
 
 
+@login_required
 def quote(request):
     tags = Tag.objects.all()
     authors = Author.objects.all()
@@ -56,3 +69,24 @@ def quote(request):
             return render(request, 'quoteapp/quote.html', {"tags": tags, 'author': authors, 'form': form})
 
     return render(request, 'quoteapp/quote.html', {"tags": tags, 'author': authors, 'form': QuoteForm()})
+
+
+@login_required
+def start_scraping(request):
+    if request.method == 'POST':
+        scraper.main()
+    return render(request, 'quoteapp/scraper.html')
+
+
+def about_author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'quoteapp/about.html', {'author': author})
+
+
+def quotes_by_tag(request, tag_name):
+    tag = get_object_or_404(Tag, name=tag_name)
+    quotes = Quote.objects.filter(tags=tag)
+    authors = Author.objects.all()
+    tag_counts = Tag.objects.annotate(quote_count=Count('quote')).order_by('-quote_count')[:10]
+    return render(request, 'quoteapp/quotes_by_tag.html', {"quotes": quotes, "tag": tag, 'author': authors, 'top_tags': tag_counts})
+
